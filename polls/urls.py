eab20d809855788c5ab88f68171d17c7e84c4725
changduchen <changduchen@163.com>
2017-07-25 10:42:43 +0800

    polls application v1.0

---
+++ polls/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    # /polls/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # /polls/5/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(),
+        name='detail'),
+    # /polls/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(),
+        name='results'),
+    # /polls/5/vote/
+    # regex:(?P<name>Experssion)  It's a capture group.
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
