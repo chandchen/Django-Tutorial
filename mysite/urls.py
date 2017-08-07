be68b8d2b0c5c46c961fbb0ad4d5b1e4726b817a
changduchen <changduchen@163.com>
2017-08-07 10:01:06 +0800

    Update Foundation

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import TemplateView
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace="polls")),
 ]
